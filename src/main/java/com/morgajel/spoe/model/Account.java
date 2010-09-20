64fa90cef43f3fe5b7182a1ddf5aa947ee8e023a
morgajel <morgajel@97df6cfb-9eb4-4c22-a9bc-2131cb5d0d81>
2010-09-20 19:34:06 +0000

    mergeing in working Doxy config
    git-svn-id: file:///home/morgajel/SPoE/spoe/trunk@124 97df6cfb-9eb4-4c22-a9bc-2131cb5d0d81

--- src/main/java/com/morgajel/spoe/model/Account.java
+++ src/main/java/com/morgajel/spoe/model/Account.java
@@ -55,7 +55,9 @@ import javax.validation.Valid;
 	)
 })
 
-
+/** 
+ * models a users interaction with the rest of the system.
+ */
 @Entity
 @Table(name="account")
 public class Account implements Serializable {
@@ -66,10 +68,22 @@ public class Account implements Serializable {
             inverseJoinColumns=
                 @JoinColumn(name="role_id", referencedColumnName="role_id")
 )
+
+	/**
+	 * returns all of the roles currently assigned to a user.
+	 **/
     public Set<Role> getRoles() { return roles; }
-	public void setRoles( Set<Role> roles) {
+    
+    /**
+     * returns all of the roles currently assigned to a user.
+     **/
+    public void setRoles( Set<Role> roles) {
 		this.roles=roles;
 	}
+    /**
+     * adds a role to the current set of Role. if roles is null, instantiates a new 
+     * HashSet and adds the role to it. 
+     **/
 	public void addRole(Role role) {
 		
 		logger.info("trying to add "+role+"to roles "+roles);
@@ -79,12 +93,21 @@ public class Account implements Serializable {
 		this.roles.add(role);
 		logger.info("added roll to "+username+", check it out:"+roles);
 	}
+	/**
+     * Adds a role to the current set of Role. if this.roles is null, instantiates a new 
+     * HashSet and adds the role to it. 
+     **/
+
 	public Set<Role> roles;
 	public static final String ALGORITHM = "SHA1";
 	public static final String PASSWDCHARSET = "!0123456789abcdefghijklmnopqrstuvwxyz";
 	private static final long serialVersionUID = -6987219647522500285L;
 	private transient static Logger logger = Logger.getLogger("com.morgajel.spoe.model.Account");
-    
+	
+	/**
+     * Takes a given string and hashes it with ALGORITHM 
+     * Same as sha() in mysql 
+     **/
 	public static String hashText(String text) {
 		//TODO factor this out into a utility class
 		StringBuilder hexStr= new StringBuilder();
@@ -105,7 +128,12 @@ public class Account implements Serializable {
 		logger.trace("Created hash "+hexStr.toString()+" from "+text);
 		return hexStr.toString();
 	}
+	
+	/**
+     * Takes a given string, hashes it, and compares it to the password; returns true if equal 
+     **/
 	public boolean verifyPassword(String password){
+		//FIXME is this still needed?
 		if (Account.hashText(password).equals(this.password)){
 			return true;
 		}
@@ -140,84 +168,125 @@ public class Account implements Serializable {
 	private Date lastAccessDate;
 	@DateTimeFormat
 	private Date creationDate;
-
+	
+	/**
+     * Constructor for Account class 
+     * disables and sets LastAccessDate and CreationDate 
+     **/
     public Account() {
     	this.enabled=false;
 	    this.setLastAccessDate(new Date());
 	    this.setCreationDate(new Date());
     }
-    
+	/**
+     * returns this.accountId, which is the Primary Key for Accounts.  
+     **/
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name="account_id")
 	public Long getAccountId() {
 		return accountId;
 	}
-	
+	/**
+     * sets this.accountId, which is the Primary Key for Accounts.  
+     **/	
 	public void setAccountId(Long accountId) {
 		this.accountId = accountId;
 	}
-
+	/**
+     * returns this.email, which is required for password recovery and account creation.  
+     **/
     @Email
     @Column(name="email", length=255, nullable=false)
 	public String getEmail() {
 		return email;
 	}
-
+    /**
+     * sets this.email, which is required for password recovery and account creation.  
+     **/
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+	/**
+     * returns this.username, which is required for login  
+     **/
     @Column(name="username")
 	public String getUsername() {
 		return username;
 	}
-
+    /**
+     * sets this.username, which is required for login  
+     **/
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	/**
+     * returns this.password, which is a hashed copy of the user's password.  
+     **/
     @Column(name="password")
 	public String getPassword() {
     	//FIXME: should I ever be able to "get" the password?
     	logger.debug("getting password:"+this.password);
 		return this.password;
 	}
-
+    /**
+     * sets this.password directly without hashing.  
+     **/
 	private void setPassword(String password) {
-		logger.debug("setting password: "+password);
+		logger.trace("setting password: "+password);
 		this.password = password;
-		logger.debug("password field is now: "+this.password);
+		logger.trace("password field is now: "+this.password);
 	}
+	/**
+     * sets this.password after hashing the given string  
+     **/
 	public void setHashedPassword(String password) {
-		logger.debug("H setting password: "+password);
+		logger.trace("H setting password: "+password);
 		setPassword(Account.hashText(password));
-		logger.debug("H password field is now: "+this.password);
+		logger.trace("H password field is now: "+this.password);
 	}
-
+	/**
+     * gets this.enabled, which is required for login.  
+     **/
     @Column(name="enabled", nullable=false )
 	public Boolean getEnabled() {
 		return enabled;
 	}
-
+	/**
+     * sets this.enabled, which is required for login.  
+     **/
 	public void setEnabled(Boolean enabled) {
 		this.enabled = enabled;
 	}
+	/**
+     * gets this.Firstname, which is rarely ever used except for addressing people  
+     **/
     @Column(name="firstname")
 	public String getFirstname() {
 		return firstname;
 	}
-
+    /**
+     * sets this.Firstname, which is rarely ever used except for addressing people  
+     **/
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+	/**
+     * gets this.Lastname, which is rarely ever used except for addressing people  
+     **/
     @Column(name="lastname")
 	public String getLastname() {
 		return lastname;
 	}
-
+	/**
+     * sets this.Lastname, which is rarely ever used except for addressing people  
+     **/
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+	/**
+     * gets this.creationDate, the date when the account was created.  
+     **/
     @Column(name="creation_date")
 	public Date getCreationDate() {
 		if (creationDate == null) {
@@ -226,10 +295,16 @@ public class Account implements Serializable {
 		return (Date) creationDate.clone();
 	}
 
-    
+    /**
+     * sets this.creationDate, which should only be used once.
+     **/
 	public void setCreationDate(Date creationDate) {
+		//TODO: need to enforce this as only happening once; perhaps final?
 		this.creationDate = (Date) creationDate.clone();
 	}
+	/**
+     * gets this.lastAccessDate, which tells when the account was last modified.  
+     **/
     @Column(name="last_access_date")
 	public Date getLastAccessDate() {
 		if (lastAccessDate == null) {
@@ -237,11 +312,17 @@ public class Account implements Serializable {
 		}
 		return (Date) lastAccessDate.clone();
 	}
-
+    /**
+     * sets this.lastAccessDate, which tells when the account was last modified.  
+     **/
 	public void setLastAccessDate(Date lastAccessDate) {
+		//TODO change this to timestamp in mysql and it will auto update
+		//FIXME should be lastModifiedDate
 		this.lastAccessDate = (Date) lastAccessDate.clone();
 	}
-
+    /**
+     * overrides the default toString, which should tell you quite a bit about your account.  
+     **/
 	@Override
 	public String toString() {
 		//FIXME: I need to update this.
@@ -259,8 +340,11 @@ public class Account implements Serializable {
 				+ ", ALGORITHM=" + ALGORITHM
 				+  "]";
 	}
-	
+	/**
+     * generates a temporary password length characters long using PASSWDCHARSET   
+     **/
 	public static String generatePassword(int length) {
+		//TODO Do I still need this?
         Random rand = new Random(System.currentTimeMillis());
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < length; i++) {
