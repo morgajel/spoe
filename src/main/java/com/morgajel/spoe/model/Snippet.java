284daa6624968990bf6a121eb21f3c4358d461f9
morgajel <morgajel@97df6cfb-9eb4-4c22-a9bc-2131cb5d0d81>
2010-10-06 03:36:57 +0000

    cleaned up snippet imports so pmd would pass.
    git-svn-id: file:///home/morgajel/SPoE/spoe/trunk@168 97df6cfb-9eb4-4c22-a9bc-2131cb5d0d81

--- src/main/java/com/morgajel/spoe/model/Snippet.java
+++ src/main/java/com/morgajel/spoe/model/Snippet.java
@@ -5,23 +5,17 @@ import java.io.Serializable;
 import javax.validation.constraints.NotNull;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.apache.log4j.Logger;
 
 import org.hibernate.annotations.NamedQueries;
-import org.hibernate.annotations.NamedQuery;
 import org.springframework.format.annotation.DateTimeFormat;
 
 /**
