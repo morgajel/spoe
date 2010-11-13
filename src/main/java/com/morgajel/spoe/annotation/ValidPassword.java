b2d5dd1691f46c6fb183ba3003491cfadba9bf5a
morgajel <morgajel@97df6cfb-9eb4-4c22-a9bc-2131cb5d0d81>
2010-11-13 04:49:47 +0000

    removed unused and duplicate imports
    git-svn-id: file:///home/morgajel/SPoE/spoe/trunk@238 97df6cfb-9eb4-4c22-a9bc-2131cb5d0d81

--- src/main/java/com/morgajel/spoe/annotation/ValidPassword.java
+++ src/main/java/com/morgajel/spoe/annotation/ValidPassword.java
@@ -1,10 +1,5 @@
 package com.morgajel.spoe.annotation;
 
-import static java.lang.annotation.ElementType.*;
-
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
--- src/main/java/com/morgajel/spoe/annotation/ValidUsername.java
+++ src/main/java/com/morgajel/spoe/annotation/ValidUsername.java
@@ -1,24 +1,17 @@
 package com.morgajel.spoe.annotation;
 
-import static java.lang.annotation.ElementType.*;
-
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.morgajel.spoe.validator.UsernameValidator;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 /**
- * 
+
  * @author jmorgan
  *
  */
